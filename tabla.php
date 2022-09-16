265589bfeacbbf51f05f14dbd21c3fe98d5ff579
Ian Santana <thief.jones@gmail.com>
2022-09-16 12:28:06 +0100

    Update tabla.php

--- tabla.php
+++ tabla.php
@@ -3,10 +3,14 @@
         <title>Prueba funcional de PHP</title>
     </head>
     <body>
-        <?php
-        $fecha = date('m-d-Y');
-        $hora = date("h:i:s a", time());
-        echo "Hoy es $fecha y son las $hora";
-        ?>
+        <table>
+            <tr>
+                <?php
+                $fecha = date('m-d-Y');
+                $hora = date("h:i:s a", time());
+                echo "Hoy es $fecha y son las $hora";
+                ?>
+            <tr>
+        </table>
     </body>
 <html>
